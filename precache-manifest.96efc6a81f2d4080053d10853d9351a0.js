0956c2eb89f218ecbf2623e433b3150d248253e1
Tina Gao <tina.hm.gao@gmail.com>
2020-02-22 18:39:00 +1300

    Updates

---
+++ precache-manifest.96efc6a81f2d4080053d10853d9351a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea2fd2a2711e0f15e846bdbe6631252",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "e425ce210d253c4e942e",
+    "url": "/star-wars/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4d4a138f0fa669e0be1d",
+    "url": "/star-wars/static/js/2.3febb35c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/star-wars/static/js/2.3febb35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e425ce210d253c4e942e",
+    "url": "/star-wars/static/js/main.389c90c8.chunk.js"
+  },
+  {
+    "revision": "bb19ec81ea61bb80c6e1",
+    "url": "/star-wars/static/js/runtime-main.73f5171a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-wars/static/media/logo.5d5d9eef.svg"
+  }
+]);
